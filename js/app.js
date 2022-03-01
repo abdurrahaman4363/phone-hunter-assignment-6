8fe2b35948413df404f0547ca2b46cf781d45868
abdurrahaman4363 <abdur.rahaman4363@gmail.com>
2022-03-01 14:03:25 +0600

    added information about phone

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+    // load data from API
+
     const loadPhone = () =>{
         const searchText = document.getElementById('search-input');
         searchResult = searchText.value;
@@ -8,18 +10,22 @@
       .then(res => res.json())
       .then(data => displayPhone(data.data))
     }
+    
+    // displaying data which is getting from API
 
     const displayPhone = PhoneDetail =>{
 
-        console.log(PhoneDetail);
+        // console.log(PhoneDetail);
 
 
         const constainerDiv = document.getElementById('container');
+        constainerDiv.textContent ="";
+
         PhoneDetail.forEach(phone => {
 
-         console.log(phone.image);
+         /* console.log(phone.image);
          console.log(phone.phone_name);
-         console.log(phone.brand);
+         console.log(phone.brand); */
 
          const div = document.createElement('div');
          div.classList.add('phone-div');
@@ -28,20 +34,87 @@
            <div class="row w-100 mx-auto">
             <div class="col m-5">
               <div class="card ">
-                <img src="${phone.image}" class="card-img-top" alt="...">
+                <img src="${phone.image}" class="card-img-top" alt="..."> 
                 <div class="card-body">
                   <h5 class="card-title">${phone.phone_name}</h5>
                   <p class="card-text">${phone.brand}</p>
-                  <button class="">phone datails</button>
+                  <button onclick="buttonDetails('${phone.slug}')" class="">phone datails</button>
                 </div>
               </div>
             </div>
           </div>
 
-         `;
+         `;//slice(0,20)
 
          constainerDiv.appendChild(div);
 
 
         })
     }
+
+    // button information
+
+const buttonDetails = (phoneInformation) =>{
+
+          //  console.log(phoneInformation);
+
+    const uri = `https://openapi.programming-hero.com/api/phone/${phoneInformation}`;
+    fetch(uri)
+    .then(res => res.json())
+    .then(data => displayPhoneDetail(data.data))
+} 
+
+// display details
+
+const displayPhoneDetail = information =>{
+  console.log(information);
+  console.log(information.mainFeatures);
+  console.log(information.others);
+  // console.log(information.mainFeatures.sensors);
+  // 
+  // console.log(information.image);
+  /* console.log(information.releaseDate);
+  console.log(information.mainFeatures);
+  
+   */
+  
+  
+  const phoneInformation = document.getElementById('phone-information');
+  phoneInformation.textContent = "";
+  const div= document.createElement('div');
+  
+  div.classList.add('card');
+
+
+
+  div.innerHTML =`
+  <img src="${information.image}" class="card-img-top" alt="...">
+  <div class="card-body">
+  <p>Name: ${information.name}</P>
+     <h3>Basic Information:</h3>
+     
+    <p>storage: ${information.mainFeatures.storage}</P>
+    <p>displaySize: ${information.mainFeatures.displaySize}</P>
+    <p>chipSet: ${information.mainFeatures.chipSet}</P> 
+    <p>memory: ${information.mainFeatures.memory}</P> 
+     
+    <p class="card-text">${information.releaseDate}</p>
+    <h3>Sensor:</h3>
+    <p>${information.mainFeatures.sensors}</p>
+     <h3>Others Information</h3>
+     <p>WLAN: ${information.others.WLAN}</p>
+    <p>Bluetooth: ${information.others.Bluetooth}</p>
+    <p>GPS: ${information.others.GPS}</p>
+    <p>NFC: ${information.others.NFC}</p>
+    <p>Radio: ${information.others.Radio}</p>
+   
+  </div>
+  `;
+
+  phoneInformation.appendChild(div);
+  
+  
+  
+}
+
+ 
